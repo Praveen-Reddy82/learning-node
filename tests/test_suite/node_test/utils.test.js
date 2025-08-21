1ecfcfc4f651fbf82d30b7f5b3e392e917aaf0f6
praveen <pr963011@gmail.com>
2025-08-21 09:17:44 +0530

    first-test-suite

---
+++ tests/test_suite/node_test/utils.test.js
@@ -0,0 +1,13 @@
+import { describe, it } from "node:test";
+import assert from "node:assert";
+import { sum, multiply } from "../utils.js";
+describe("Utils Test Suite: sum", () => {
+it("Should sum two numbers", () => {
+assert.strictEqual(sum(1, 2), 3);
+});
+});
+describe("Utils Test Suite: multiply", () => {
+it("Should multiply two numbers", () => {
+assert.strictEqual(multiply(5, 3), 15);
+});
+});
